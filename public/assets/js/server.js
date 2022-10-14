58227d066f6bde0f7f0803ccbb84d725fc26cdf3
Grant-Ellington <gellingtonem6@gmail.com>
2022-10-14 14:52:58 -0500

    save pint started connecting get functionality

---
+++ public/assets/js/server.js
@@ -0,0 +1,19 @@
+
+//requirement for npm express
+const express = require('express')
+//setting up common functions for exporess
+const app = express();
+
+//created PORT vairibale for .listen 
+const PORT = 3002
+const path = require('path')
+//middleware
+app.use(express.static('public'))
+
+app.get('notes', (req, res)=>{
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+})
+
+//set up for port to be listened to by local host
+app.listen(PORT, ()=>
+console.log(`Serving static asset routes at http://localhost:${PORT}!`))
